f3bb752edd015bae90ecd05d609f18383b7c1212
karan soni <karanparekh1011@gmail.com>
2025-03-22 22:40:29 +0530

    Add files via upload

---
+++ DATATYPE.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	cout<<sizeof(int)<<endl;
+	cout<<sizeof(float)<<endl;
+	cout<<sizeof(double)<<endl;
+	cout<<sizeof(bool)<<endl;
+	cout<<sizeof(char)<<endl;
+	cout<<sizeof(string);
+	getch();
+	return 0;
+}
