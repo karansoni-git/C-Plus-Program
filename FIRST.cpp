30eb28f1dc2858521b14dae5b094701ce9950847
karan soni <karanparekh1011@gmail.com>
2025-03-23 22:36:31 +0530

    Add files via upload

---
+++ FIRST.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int a;
+	cout<<"hello all\n";//std::cout<<"hello all";this uses when we dont use this -->using namespace std;
+	cout<<"enter a number:";
+	cin>>a;
+	cout<<"number is:";
+	cout<<a;
+	getch();
+	return 0;
+}
+//<<endl -->use for new line.also use \n
+//<<ends -->use for space.
