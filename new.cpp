d2a2c6d844bc0b5b28a3bd460c3d635320029ba2
karan soni <karanparekh1011@gmail.com>
2025-03-29 23:43:36 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class k
+{
+	int x;
+	string k;
+	char a;
+};
+int main()
+{
+	//memory distribution work same as structure has.
+	cout<<sizeof(k);
+	getch();
+	return 0;
+}
