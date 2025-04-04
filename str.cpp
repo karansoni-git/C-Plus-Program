f56947e2d3fd3ae7919f8e44ff302a0618306713
karan soni <karanparekh1011@gmail.com>
2025-04-04 23:41:50 +0530

    Add files via upload

---
+++ str.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+main()
+{
+//	char name[20];
+//	cout<<"enter your full name:";
+//	gets(name);
+//	puts(name);
+//	cout<<endl;
+	
+//string is one type of class which has object see below example.
+//	string address; //string is data type in c++
+//	cout<<"enter your address:";
+//	getline(cin,address);//getline is function to input string it's use only when data type is string
+//	cout<<address;
+	
+//	string a="karan soni";
+//	cout<<"original:"<<a<<endl;
+//	a.push_back('s');//push_back function add single character at end of string
+//	a.pop_back();//this function pop single character from string.
+//	cout<<a;
+
+//inbuilt string function
+//char name[]="karan";
+//char name2[]="Karan";
+//char sr[10];
+//strcpy(sr,"soni");//strcpy function use for copy one string into other or input string in specific variable
+//cout<strcat(name,sr);//strcat use to concatenate the two string.
+//cout<<name<<endl;
+//cout<<"length of string:";
+//cout<<strlen(name);//find the length of string
+//cout<<strrev(name);//reverse the string.
+//int a=strcmp(name,name2);//this function return 0 or 1.
+//if(a==0)
+//	{
+//		cout<<"string is same";
+//	}
+//else
+//	{
+//		cout<<"string is not same";
+//	}
+
+//function for string object.
+//string fullname="karan soni";
+//cout<<fullname.size()<<endl;//find length
+//cout<<fullname.length()<<endl;//also find length
+//fullname.replace(starting-point,total-length,new-string);
+//cout<<fullname.replace(6,fullname.length(),"parekh");//replace the work from string can input new word to string
+//cout<<fullname.append(" sanjaybhai");//concate the string.
+//reverse(fullname.begin() , fullname.end());//reversing the string
+//cout<<fullname;
+
+getch();
+return 0;
+}
